22f288c30ee75574347f1ea7fb37d3bf335dc705
Lisa Weber <lcw5030@gmail.com>
2013-09-11 21:03:42 -0400

    Added my short bio to the page

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Welcome to P1
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dynamic Web Applications</title>
+	</head>
+	<body id="bodyText">
+		<p> My Name is Lisa Weber.
+			<br>
+			I am currently a software developer at Liberty Mutual. I graduated in 2011 from Penn State with a B.S. in Mechanical Engineering and a minor in Spanish. 
+			<br>
+			In my free time I enjoy volunteering at the SPCA, watching football, and playing volleyball. I am most passionate about running and have completed two half-marathons and a full marathon this year.
+		</p>
+	</body>
+</html>

7c84944093fe92b044a08d3c75d5762f39271fcc
Lisa Weber <lcw5030@gmail.com>
2013-09-11 22:02:03 -0400

    Added the css style sheet

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 	<head>
 		<title>Dynamic Web Applications</title>
 	</head>
-	<body id="bodyText">
-		<p> My Name is Lisa Weber.
+	<body>
+		<p class="bodyText"> My Name is Lisa Weber.
 			<br>
 			I am currently a software developer at Liberty Mutual. I graduated in 2011 from Penn State with a B.S. in Mechanical Engineering and a minor in Spanish. 
 			<br>
 			In my free time I enjoy volunteering at the SPCA, watching football, and playing volleyball. I am most passionate about running and have completed two half-marathons and a full marathon this year.
+		</p>
+			<br>
+			<br>
+			<br>
+		<p class="bodyText">
+			The code editor I am using is Sublime Text
 		</p>
 	</body>
 </html>
